ad7e90c471ad63cf126be82ae6a3a528bd3d1894
admin <admin@Macbook-Pro-cua-Kinebeo.local>
2024-10-03 14:51:56 +0700

    feat: add core packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import {nextui} from '@nextui-org/theme';
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -5,15 +6,39 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
     },
+    screens: {
+      mobile: {
+        min: "100px",
+        max: "550px",
+      },
+      tablet: {
+        min: "550px",
+        max: "750px",
+      },
+      "mini-laptop": {
+        min: "750px",
+        max: "874px",
+      },
+      laptop: {
+        min: "874px",
+        max: "1280px",
+      },
+      desktop: {
+        min: "1280px",
+      },
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
 export default config;

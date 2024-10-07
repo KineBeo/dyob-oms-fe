d6ff47bb26e9333003d145a0091ebef7492e8bac
NguyenVanHung2004 <125149885+NguyenVanHung2004@users.noreply.github.com>
2024-10-07 17:12:41 +0700

    feat: test permission

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -120,7 +120,7 @@ export default function HeroSection() {
                 href={item.href}
                 className="text-text-brown-primary hover:text-yellow-600 px-3 py-2 rounded-md font-medium text-2xl laptop:text-lg"
               >
-                <div className="flex justify-between items-center text-text-brown-primary">
+                <div className="flex justify-between items-center ">
                   {item.title}
                 </div>
               </Link>
@@ -130,7 +130,7 @@ export default function HeroSection() {
 
         <NavbarMenu className="w-full h-1/3 pt-6">
           {menuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
+            <NavbarMenuItem  key={`${item}-${index}`}>
               <Link
                 color={
                   index === 2
@@ -149,6 +149,7 @@ export default function HeroSection() {
           ))}
           <div className="h-3 bg-navbar-line w-full fixed bottom-2/3 left-0 "></div>
         </NavbarMenu>
+        
       </Navbar>
       <div className="h-4 mobile:h-3 tablet:h-3 mini-laptop:h-3  bg-navbar-line w-full"></div>
     </div>

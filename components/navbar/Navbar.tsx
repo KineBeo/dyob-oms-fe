7eea2a77627bd001fe1c0360990cc3cf7c748554
NguyenVanHung2004 <125149885+NguyenVanHung2004@users.noreply.github.com>
2024-10-07 17:17:07 +0700

    fix:deploy

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,14 +1,12 @@
 "use client";
 import Image from "next/image";
 import { useState } from "react";
-import { IoMdMenu } from "react-icons/io";
 import {
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
   Link,
-  Button,
   NavbarMenuToggle,
   NavbarMenu,NavbarMenuItem
 } from "@nextui-org/react";
@@ -25,69 +23,6 @@ export default function HeroSection() {
   ];
   return (
     <div className="bg-navbar-background">
-      {/* Desktop  */}
-      {/* <nav className="bg-navbar-background shadow-md">
-        <div className="max-w-7xl mx-auto px-4">
-          <div className="flex justify-between h-20">
-            <div className="flex items-center ">
-              <div className="flex-shrink-0 flex items-center">
-                <Image
-                  src="/images/logo-image.png"
-                  alt="Đông Y Ông Bụt Logo"
-                  width={64}
-                  height={64}
-                  className="h-16 w-auto"
-                />
-                <div className="ml-2">
-                  <div className="text-lg font-medium text-text-brown-primary">
-                    Quán tâm an bệnh
-                  </div>
-                  <div className="text-2xl font-bold text-text-brown-primary">
-                    Đông Y Ông Bụt
-                  </div>
-                </div>
-              </div>
-              <IoMdMenu
-                className="md:hidden w-10 h-10 text-[#D4A45F] ml-20"
-                onClick={() => setIsOpen(!isOpen)}
-              />
-            </div>
-            <div className="flex items-center">
-              <div className="hidden mini-laptop:flex laptop:flex desktop:flex space-x-8">
-                {menuItems.map((item, index) => (
-                  <Link
-                    href={item.href}
-                    className="text-text-brown-primary hover:text-yellow-600 px-3 py-2 rounded-md font-medium text-2xl"
-                  >
-                    <div className="flex justify-between items-center">
-                      {item.title}
-                    </div>
-                  </Link>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="h-4 bg-navbar-line"></div>
-      </nav> */}
-      {/* Mobilie */}
-      {/* <div className="mini-laptop:hidden laptop:hidden desktop:hidden ">
-        <div className={`bg-[#FDF5E6] ${isOpen ? "block" : "hidden"}`}>
-          {menuItems.map((item, index) => (
-            <div key={index} className="border-b border-[#E6CCB3]">
-              <Link
-                href={item.href}
-                className="block p-4 text-text-brown-primary hover:bg-[#FFE4B5]"
-              >
-                <div className="flex justify-between items-center font-medium">
-                  {item.title}
-                  {item.hasSubmenu && <span className="text-sm">▼</span>}
-                </div>
-              </Link>
-            </div>
-          ))}
-        </div>
-      </div> */}
       <Navbar onMenuOpenChange={setIsOpen} className="py-1 px-0">
         <NavbarContent className=" desktop:-ml-20">
           <NavbarBrand>
@@ -115,7 +50,7 @@ export default function HeroSection() {
 
         <NavbarContent className="hidden laptop:flex desktop:flex gap-4 ">
           {menuItems.map((item, index) => (
-            <NavbarItem>
+            <NavbarItem key={index}>
               <Link
                 href={item.href}
                 className="text-text-brown-primary hover:text-yellow-600 px-3 py-2 rounded-md font-medium text-2xl laptop:text-lg"
@@ -130,7 +65,7 @@ export default function HeroSection() {
 
         <NavbarMenu className="w-full h-1/3 pt-6">
           {menuItems.map((item, index) => (
-            <NavbarMenuItem  key={`${item}-${index}`}>
+            <NavbarMenuItem key={`${item}-${index}`}>
               <Link
                 color={
                   index === 2
@@ -149,7 +84,6 @@ export default function HeroSection() {
           ))}
           <div className="h-3 bg-navbar-line w-full fixed bottom-2/3 left-0 "></div>
         </NavbarMenu>
-        
       </Navbar>
       <div className="h-4 mobile:h-3 tablet:h-3 mini-laptop:h-3  bg-navbar-line w-full"></div>
     </div>

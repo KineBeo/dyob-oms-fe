2669b22de99b974971775f676db85500f2272e48
idontwannapickaname <lightrageg@gmail.com>
2024-10-08 15:53:50 +0700

    adding card

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,7 +1,36 @@
+import OurDestinyCard from "@/components/cards/OurDestinyCard";
+import ProductCard from "@/components/cards/ProductCard";
+import StoreCard from "@/components/cards/StoreCard";
+import WhyChoosingCard from "@/components/cards/WhyChoosingCard";
+
 export default function Search() {
     return (
-        <div>
-            Search
+        <div className="grid
+        mobile:grid-cols-2
+        tablet:grid-cols-2
+        mini-laptop:grid-cols-3
+        laptop:grid-cols-3
+        desktop:grid-cols-3
+        justify-items-center bg-slate-600">
+            {/* <StoreCard image_url="/images/shop.png" location="Đông Y Ông Bụt Hà Nội" />
+            <StoreCard image_url="/images/shop.png" location="Đông Y Ông Bụt Hồ Chí Minh" /> */}
+
+            {/* <ProductCard image_url="/images/product.png" title="An vi khang ong but" price="200000d" />
+            <ProductCard image_url="/images/product.png" title="An vi khang ong but" price="200000d" />
+            <ProductCard image_url="/images/product.png" title="An vi khang ong but" price="200000d" />
+            <ProductCard image_url="/images/product.png" title="An vi khang ong but" price="200000d" /> */}
+
+            {/* <WhyChoosingCard image_url="/images/whychoosing.png" title="Duoc lieu sach 200%" />
+            <WhyChoosingCard image_url="/images/whychoosing.png" title="Duoc lieu sach 200%" />
+            <WhyChoosingCard image_url="/images/whychoosing.png" title="Duoc lieu sach 200%" />
+            <WhyChoosingCard image_url="/images/whychoosing.png" title="Duoc lieu sach 200%" />
+            <WhyChoosingCard image_url="/images/whychoosing.png" title="Duoc lieu sach 200%" /> */}
+
+            <OurDestinyCard />
+            <OurDestinyCard />
+            <OurDestinyCard />
         </div>
+
+        
     )
 }
---
+++ components/cards/OurDestinyCard.tsx
@@ -0,0 +1,42 @@
+import { Card, CardHeader, CardBody, CardFooter, Divider, Link } from "@nextui-org/react";
+import Image from "next/image";
+import { IoMdGlobe } from "react-icons/io";
+import { RiCheckboxCircleLine } from "react-icons/ri";
+
+
+export default function OurDestinyCard() {
+    return (
+        <Card className="m-2 w-fit rounded-none">
+            <CardHeader className="justify-between px-6">
+                <p className="text-lg font-semibold">NextUI</p>
+                    <IoMdGlobe className="text-[#7A0505]"/>
+            </CardHeader>
+            <Divider className="bg-[#7A0505] w-4/5 place-self-center " />
+            <CardBody>
+                <div className="justify-start px-2 ">
+                    <div className="flex my-4 gap-1">
+                        <RiCheckboxCircleLine className="size-fit text-[#7A0505] "/>
+                        <p className="text-xs capitalize w-fit font-medium"> lay dao phat lam con duong phat trien </p>
+                    </div>
+                    <div className="flex my-4 gap-1">
+                        <RiCheckboxCircleLine className="size-fit text-[#7A0505] " />
+                        <p className="text-xs capitalize w-fit font-medium"> lay dao phat lam con duong phat trien </p>
+                    </div>
+                    <div className="flex my-4 gap-1">
+                        <RiCheckboxCircleLine className="size-fit text-[#7A0505] " />
+                        <p className="text-xs capitalize w-fit font-medium"> lay dao phat lam con duong phat trien </p>
+                    </div>
+                    <div className="flex my-4 gap-1">
+                        <RiCheckboxCircleLine className="size-fit text-[#7A0505] " />
+                        <p className="text-xs capitalize w-fit font-medium"> lay dao phat lam con duong phat trien </p>
+                    </div>
+                    <div className="flex my-4 gap-1 ">
+                        <RiCheckboxCircleLine className="size-fit text-[#7A0505] " />
+                        <p className="text-xs capitalize w-fit font-medium"> lay dao phat lam con duong phat trien </p>
+                    </div>
+                </div>
+            </CardBody>
+            <Divider className="bg-[#7A0505] h-1 mt-8" />
+        </Card>
+    )
+}

83a9b68243e94e92d66c492fe9c464fc7d79bb54
idontwannapickaname <lightrageg@gmail.com>
2024-10-10 20:38:01 +0700

    feat: add font style, bg image

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,7 @@
 import ProductCard from "@/components/cards/ProductCard";
 import { Card } from "@/components/ui/card";
 import { Pagination } from "@nextui-org/react";
+import Image from "next/image";
 
 import {
     Carousel,
@@ -12,38 +13,37 @@ import {
 
 export default function Products() {
     return (
-        <div>
-            <div className="place-content-center w-full mobile:h-32 h-48 bg-red-900">
-                <p className="text-center font-bold text-2xl"> Đông Y Ông bụt</p>
-                <p className="text-center font-bold">Quán tâm an bệnh</p>
+        <div className="bg-paper">
+            <div className="place-content-center w-full mobile:h-32 tablet:h-40 h-48 bg-[#3F291B]">
+
             </div>
 
 
-            <div className="my-8 text-center font-semibold font-serif">
+            <div className="my-8 text-center font-extrabold font-robotoslab text-[#3F291B]">
                 <p className="text-xl">Sản phẩm</p>
-                <p className="text-2xl">Đông y ông bụt</p>
+                <p className="text-2xl uppercase">Đông y ông bụt</p>
             </div>
 
             <div className="my-4">
                 {/* Desktop */}
-                <div className="justify-center items-center flex bg-red-500 mobile:hidden tablet:hidden">
+                <div className="justify-center items-center flex mobile:hidden tablet:hidden">
                     <Carousel
                         opts={{
                             align: "start"
                         }}
-                        className="w-full mini-laptop:max-w-2xl laptop:max-w-[50rem] max-w-6xl">
+                        className="w-full mini-laptop:max-w-2xl laptop:max-w-[52rem] max-w-6xl">
                         <CarouselContent>
                             {Array.from({ length: 10 }).map((_, index) => (
                                 <CarouselItem key={index} className="mini-laptop:basis-1/3 basis-1/4 grid grid-rows-2">
                                     <div className="p-1">
-                                        <Card>
-                                            <ProductCard image_url="/images/product.png" title="An vị khang Ông Bụt" price="200.000đ" />
-                                        </Card>
+
+                                        <ProductCard image_url="/images/product.png" title="An vị khang Ông Bụt" price="200.000đ" />
+
                                     </div>
                                     <div className="p-1">
-                                        <Card>
-                                            <ProductCard image_url="/images/product.png" title="Than y Ông Bụt" price="200.000đ" />
-                                        </Card>
+
+                                        <ProductCard image_url="/images/product.png" title="Than y Ông Bụt" price="200.000đ" />
+
                                     </div>
                                 </CarouselItem>
                             ))}
@@ -55,9 +55,9 @@ export default function Products() {
                 </div>
             </div>
 
-            <div className="flex flex-shrink-0 overflow-x-auto items-center my-4 bg-red-500 mini-laptop:hidden laptop:hidden desktop:hidden">
-                 {/* Mobile Tablet */}
-                <div className="flex flex-shrink-0 overflow-x-scroll">
+            <div className="flex flex-shrink-0 overflow-x-auto items-center mx-4 my-4 mini-laptop:hidden laptop:hidden desktop:hidden">
+                {/* Mobile Tablet */}
+                <div className="flex flex-shrink-0">
                     <ProductCard image_url="/images/product.png" title="An vị khang Ông Bụt" price="200.000đ" />
                     <ProductCard image_url="/images/product.png" title="An khang Ông Bụt" price="200.000đ" />
                     <ProductCard image_url="/images/product.png" title="vị khang Ông Bụt" price="200.000đ" />
@@ -68,7 +68,7 @@ export default function Products() {
                 </div>
             </div>
 
-            <Pagination className="flex justify-center items-center" total={10} initialPage={1} />
+            <Pagination className="flex justify-center items-center my-4" total={10} initialPage={1} />
         </div>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,47 +5,56 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			'text-brown-primary': '#3C2415',
-  			'navbar-background': '#FBF6EC',
-  			'navbar-line': '#D7A444'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	},
-  	screens: {
-  		mobile: {
-  			min: '100px',
-  			max: '550px'
-  		},
-  		tablet: {
-  			min: '550px',
-  			max: '750px'
-  		},
-  		'mini-laptop': {
-  			min: '750px',
-  			max: '874px'
-  		},
-  		laptop: {
-  			min: '874px',
-  			max: '1280px'
-  		},
-  		desktop: {
-  			min: '1280px'
-  		},
-  		sm: '640px',
-  		md: '768px',
-  		lg: '1024px',
-  		xl: '1280px',
-  		'2xl': '1536px'
-  	}
+    extend: {
+      colors: {
+        "text-brown-primary": "#3C2415",
+        "navbar-background": "#FBF6EC",
+        "navbar-line": "#D7A444",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        robotoslab: ["Roboto Slab", "serif"],
+        robotoflex: ["Roboto Flex", "sans - serif"],
+        inter: ["Inter", "sans - serif"],
+      },
+      backgroundImage: {
+        paper: "url('/images/productbg.png')",
+      },
+    },
+    screens: {
+      mobile: {
+        min: "100px",
+        max: "550px",
+      },
+      tablet: {
+        min: "550px",
+        max: "750px",
+      },
+      "mini-laptop": {
+        min: "750px",
+        max: "874px",
+      },
+      laptop: {
+        min: "874px",
+        max: "1280px",
+      },
+      desktop: {
+        min: "1280px",
+      },
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   darkMode: ["class", "class"],
   plugins: [nextui(), require("tailwindcss-animate")],

8fa29346ac8605528d1644f8abe684e41d7abee2
NguyenVanHung2004 <125149885+NguyenVanHung2004@users.noreply.github.com>
2024-10-11 17:01:22 +0700

    fix: deploy error

--- components/specific-product-page/MoreDetails.tsx
+++ components/specific-product-page/MoreDetails.tsx
@@ -14,9 +14,9 @@ const MoreDetails = () => {
             </h3>
             <p className="text-gray-700">
               Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
+              industry. Lorem Ipsum has been the industry&apos;s standard dummy
+              text ever since the 1500s, when an unknown printer took a galley
+              of type and scrambled it to make a type specimen book. It has
               survived not only five centuries, but also the leap into
               electronic typesetting,
             </p>
@@ -28,9 +28,9 @@ const MoreDetails = () => {
             </h3>
             <p className="text-gray-700">
               Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
+              industry. Lorem Ipsum has been the industry&apos;s standard dummy
+              text ever since the 1500s, when an unknown printer took a galley
+              of type and scrambled it to make a type specimen book. It has
               survived not only five centuries, but also the leap into
               electronic typesetting,
             </p>
@@ -42,9 +42,9 @@ const MoreDetails = () => {
             </h3>
             <p className="text-gray-700">
               Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
+              industry. Lorem Ipsum has been the industry&apos;s standard dummy
+              text ever since the 1500s, when an unknown printer took a galley
+              of type and scrambled it to make a type specimen book. It has
               survived not only five centuries, but also the leap into
               electronic typesetting,
             </p>

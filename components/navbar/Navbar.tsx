ed8b0d067f555eab4df205a759833df2703c053c
NguyenVanHung2004 <125149885+NguyenVanHung2004@users.noreply.github.com>
2024-10-14 15:41:16 +0700

    refactor: refactor navbar things

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
+import { useRouter } from "next/navigation";
+import React from "react";
 import Image from "next/image";
-import { useState } from "react";
 import {
   Navbar,
   NavbarBrand,
@@ -8,84 +9,95 @@ import {
   NavbarItem,
   Link,
   NavbarMenuToggle,
-  NavbarMenu,NavbarMenuItem
+  NavbarMenu,
+  NavbarMenuItem,
+  Divider,
 } from "@nextui-org/react";
 
 export default function HeroSection() {
-  const [isOpen, setIsOpen] = useState(false);
-
+  const [isOpen, setIsOpen] = React.useState(false);
+  const router = useRouter();
   const menuItems = [
     { title: "Trang chủ", href: "/" },
-    { title: "Về chúng tôi", href: "/#", hasSubmenu: true },
-    { title: "Sản phẩm", href: "/#", hasSubmenu: true },
-    { title: "Phản hồi", href: "/#" },
-    { title: "Giỏ hàng", href: "/#" },
+    { title: "Về chúng tôi", href: "/about-us-normal", hasSubmenu: true },
+    { title: "Sản phẩm", href: "/products", hasSubmenu: true },
+    { title: "Affiliate", href: "/affiliate" },
+    { title: "Giỏ hàng", href: "/cart" },
   ];
   return (
-    <div className="bg-navbar-background">
-      <Navbar onMenuOpenChange={setIsOpen} className="py-1 px-0">
-        <NavbarContent className=" desktop:-ml-20">
-          <NavbarBrand>
+    // <div className="bg-navbar-background">
+    <Navbar
+      onMenuOpenChange={setIsOpen}
+      className="py-1 px-0 bg-navbar-background mini-laptop:border-b-8 laptop:border-b-8 desktop:border-b-8 border-[#D7A444]"
+      maxWidth="2xl"
+      shouldHideOnScroll
+    >
+      <NavbarContent>
+        <NavbarBrand
+          onClick={() => router.push("/")}
+          className="hover:cursor-pointer"
+        >
+          <div>
             <Image
               src="/images/logo-image.png"
               alt="Đông Y Ông Bụt Logo"
-              width={64}
-              height={64}
-              className="h-auto w-auto mobile:h-14 mobile:w-14"
+              width={48}
+              height={48}
+              priority
+              loading="eager"
+              className="h-auto w-auto mobile:h-14 mobile:w-14 hover:scale-110 transition"
             />
-            <div className="ml-2">
-              <div className="text-lg  laptop:text-medium mini-laptop:text-medium tablet:text-medium mobile:text-medium font-medium text-text-brown-primary">
-                Quán tâm an bệnh
-              </div>
-              <div className="text-2xl laptop:text-large mini-laptop:text-large tablet:text-large mobile:text-large font-bold text-text-brown-primary">
-                Đông Y Ông Bụt
-              </div>
+          </div>
+          <div className="ml-2">
+            <div className="mobile:text-sm tablet:text-md mini-laptop:text-md laptop:text-lg desktop:text-lg text-text-brown-primary">
+              Quán tâm an bệnh
             </div>
-          </NavbarBrand>
-          <NavbarMenuToggle
-            aria-label={isOpen ? "Close menu" : "Open menu"}
-            className="laptop:hidden desktop:hidden"
-          />
-        </NavbarContent>
+            <div className="mobile:text-md tablet:text-lg mini-laptop:text-lg laptop:text-xl desktop:text-xl font-semibold text-text-brown-primary">
+              Đông Y Ông Bụt
+            </div>
+          </div>
+        </NavbarBrand>
+        <NavbarMenuToggle
+          aria-label={isOpen ? "Close menu" : "Open menu"}
+          className="laptop:hidden desktop:hidden"
+        />
+      </NavbarContent>
 
-        <NavbarContent className="hidden laptop:flex desktop:flex gap-4 ">
-          {menuItems.map((item, index) => (
-            <NavbarItem key={index}>
-              <Link
-                href={item.href}
-                className="text-text-brown-primary hover:text-yellow-600 px-3 py-2 rounded-md font-medium text-2xl laptop:text-lg"
-              >
-                <div className="flex justify-between items-center ">
-                  {item.title}
-                </div>
-              </Link>
-            </NavbarItem>
-          ))}
-        </NavbarContent>
-
-        <NavbarMenu className="w-full h-1/3 pt-6">
-          {menuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === menuItems.length - 1
-                    ? "danger"
-                    : "foreground"
-                }
-                className="w-full"
-                href="#"
-                size="lg"
-              >
+      <NavbarContent
+        className="hidden laptop:flex desktop:flex gap-6"
+        justify="end"
+      >
+        {menuItems.map((item, index) => (
+          <NavbarItem key={index}>
+            <Link
+              href={item.href}
+              className="text-text-brown-primary hover:text-[#D7A444] font-medium laptop:text-lg desktop:text-xl "
+            >
+              <div className="flex justify-between items-center">
                 {item.title}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-          <div className="h-3 bg-navbar-line w-full fixed bottom-2/3 left-0 "></div>
-        </NavbarMenu>
-      </Navbar>
-      <div className="h-4 mobile:h-3 tablet:h-3 mini-laptop:h-3  bg-navbar-line w-full"></div>
-    </div>
+              </div>
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+
+      <NavbarMenu className="w-full h-1/3 pt-6 bg-navbar-background">
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color="foreground"
+              className="w-full font-semibold text-text-brown-primary"
+              href={item.href}
+              size="md"
+            >
+              <div className="w-full">
+                {item.title}
+                <Divider className="my-2 bg-[#D7A444]" />
+              </div>
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
   );
 }

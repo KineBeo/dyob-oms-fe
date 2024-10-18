604cfd2bfed57db0c72e823c5b83b37a9dd9df8a
NguyenVanHung2004 <125149885+NguyenVanHung2004@users.noreply.github.com>
2024-10-18 15:15:38 +0700

    fix: contact icons

--- components/ContactIcon.tsx
+++ components/ContactIcon.tsx
@@ -5,7 +5,7 @@ import {
   FaPhone,
 } from "react-icons/fa";
 import Link from "next/link";
-
+import { SiZalo } from "react-icons/si";
 const ContactIcons = () => {
   return (
     <>
@@ -42,15 +42,14 @@ const ContactIcons = () => {
       <div className="fixed right-4 bottom-4 flex flex-col gap-3 z-50">
         <Link
           href="mailto:contact@example.com"
-          className="w-10 h-10 rounded-full bg-blue-500 flex items-center justify-center hover:bg-blue-600 transition-colors"
+          className="w-10 h-10 mobile:w-10 mobile:h-10 rounded-full bg-blue-500 flex items-center justify-center hover:bg-blue-600 hover:scale-110 transition-colors"
           aria-label="Email"
         >
-          <FaEnvelope className="text-white text-2xl" />
+          <SiZalo className="text-white text-2xl" />
         </Link>
-
         <Link
           href="https://m.me/yourpage"
-          className="w-10 h-10 rounded-full bg-blue-500 flex items-center justify-center hover:bg-blue-600 transition-colors"
+          className="w-10 h-10 rounded-full bg-blue-500 flex items-center justify-center hover:bg-blue-600 hover:scale-110  transition-colors"
           aria-label="Messenger"
         >
           <FaFacebookMessenger className="text-white text-2xl" />
@@ -60,7 +59,7 @@ const ContactIcons = () => {
           <div className="ripple-container">
             <Link
               href="tel:+1234567890"
-              className="w-10 h-10 rounded-full bg-amber-500 flex items-center justify-center hover:bg-amber-600 transition-colors relative z-10"
+              className="w-10 h-10 rounded-full bg-amber-500 flex items-center justify-center hover:bg-amber-600 transition-colors hover:scale-110 relative z-10"
               aria-label="Phone"
             >
               <FaPhone className="text-white text-2xl" />

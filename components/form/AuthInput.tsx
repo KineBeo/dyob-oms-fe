7658bd0ba13f5d67bb8c098470484f023371ffd3
TruongHoang2004 <22028035@vnu.edu.vn>
2024-10-27 23:01:10 +0700

    feat: add raw login and register form

---
+++ components/form/AuthInput.tsx
@@ -0,0 +1,29 @@
+interface AuthInputProps {
+    id: string;
+    label: string;
+    type: string;
+    placeholder: string;
+}
+
+export default function AuthInput({ id, label, type, placeholder }: AuthInputProps) {
+
+    const inputType = type === "phone" ? "tel" : type;
+
+    return (
+        <div className="mb-4">
+            <label htmlFor={id} className="block font-medium text-black text-sm">
+                {label}
+                <span className="text-[#7A0505]"> *</span>
+            </label>
+            <div className="relative flex mt-1">
+                <input
+                    type={type}
+                    id={id}
+                    className="border-[#7A0505] bg-[#EDEFFE] shadow-sm p-4 pr-4 border w-full text-sm"
+                    placeholder={placeholder}
+                    pattern={type === "tel" ? "[0-9]{10}" : undefined}
+                />
+            </div>
+        </div>
+    );
+}
